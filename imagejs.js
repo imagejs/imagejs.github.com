59dad1dba5c12f73c7c295876f233cbeb6bba9c0
Jonas Almeida <jalmeida@mathbiol.org>
2012-01-11 15:46:10 -0600

    testing dual commit

--- imagejs.js
+++ imagejs.js
@@ -33,7 +33,7 @@ readImage:function(f){ // read image file
 			cvTop.height=cvBase.height;
 			cvTop.style.position='absolute';
 			cvTop.style.cursor='crosshair';
-			cvBase.style.cursor='crosshair';
+			//cvBase.style.cursor='crosshair';
 			cvTop.style.left=cvBase.offsetLeft;
 			//cvTop.style.top=cvBase.offsetTop;
 			jmat.gId('work').appendChild(cvTop);		
