0371f2388b3168244a1c6c67b727c74e3e60c575
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-04-12 09:48:31 -0500

    Remove duplicated LZW algorihm.

--- mainMenu.js
+++ mainMenu.js
@@ -16,7 +16,7 @@ function handleDownloadImage(ev) {
 
 function handleJSONDownload(){
     var jsonText = JSON.stringify(imagejs.data);
-    var compressedText = LZW.compress(jsonText); 
+    var compressedText = LZW.compress(jsonText);
 
     var bb = new WebKitBlobBuilder();
     bb.append(compressedText);
@@ -28,11 +28,11 @@ function handleJSONDownload(){
 
 
 function handleJSONDownload2(){
-	var imageURI = imagejs.data.orig;
-	var seperatorString = "ZZZZZZZ"
+    var imageURI = imagejs.data.orig;
+    var seperatorString = "ZZZZZZZ"
     var segText = JSON.stringify(imagejs.data.seg);
-	var compressedSegText = LZW.compress(segText);
-	var text = imageURI + seperatorString + compressedSegText;
+    var compressedSegText = LZW.compress(segText);
+    var text = imageURI + seperatorString + compressedSegText;
 
     var bb = new WebKitBlobBuilder();
     bb.append(text);
@@ -41,13 +41,13 @@ function handleJSONDownload2(){
     oURL = oURL.createObjectURL(b);
     document.getElementById('jdButton').href = oURL;
 }
- 
+
 
 (function(){
     var listOfModules={
         'Hello world':function(evt){imagejs.loadModule('http://imagejs.googlecode.com/git/helloWorld.js')},
         'Chromomarkers':function(evt){imagejs.loadModule('http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js')},
-        'Count Shapes':function(evt){imagejs.loadModules(['http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','http://module.imagejs.googlecode.com/git/mathbiol.countshapes.js'])}           
+        'Count Shapes':function(evt){imagejs.loadModules(['http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','http://module.imagejs.googlecode.com/git/mathbiol.countshapes.js'])}
     }
 
     var menu={
@@ -62,13 +62,13 @@ function handleJSONDownload2(){
             console.log('Save Results');
         }
     }
-   
+
 
     var name= 'Main Menu';
     jmat.gId('menu').appendChild(imagejs.menu(menu,name)); // <-- this
     cvTop.style.left=cvBase.offsetLeft;cvTop.style.top=cvBase.offsetTop; // make sure the two canvas are aligned
     // --------------
-   
+
 
     var a = document.createElement('a');
     document.body.appendChild(a);
@@ -78,7 +78,7 @@ function handleJSONDownload2(){
     a.addEventListener('click', handleDownloadImage, false);
 
     document.body.appendChild(document.createElement('br'))
-   
+
     var b = document.createElement('a');
     document.body.appendChild(b);
     b.id = "jdButton";
@@ -86,81 +86,81 @@ function handleJSONDownload2(){
     b.download="file.txt";
     b.textContent="Download File";
     b.addEventListener('click', handleJSONDownload2, false);
-   
+
 })()
 
+// Source: http://rosettacode.org/wiki/LZW_compression#JavaScript
 var LZW = {
-                compress: function (uncompressed) {
-                    "use strict";
-                    // Build the dictionary.
-                    var i,
-                        dictionary = {},
-                        c,
-                        wc,
-                        w = "",
-                        result = [],
-                        dictSize = 256;
-                    for (i = 0; i < 256; i += 1) {
-                        dictionary[String.fromCharCode(i)] = i;
-                    }
-             
-                    for (i = 0; i < uncompressed.length; i += 1) {
-                       
-                        c = uncompressed.charAt(i);
-                        wc = w + c;
-                        if (dictionary[wc]) {
-                            w = wc;
-                        } else {
-                            result.push(dictionary[w]);
-                            // Add wc to the dictionary.
-                            dictionary[wc] = dictSize++;
-                            w = String(c);
-                        }
-                    }
-             
-                    // Output the code for w.
-                    if (w !== "") {
-                        result.push(dictionary[w]);
-                    }
-                    return result;
-                },
-             
-             
-                decompress: function (compressed) {
-                    "use strict";
-                    // Build the dictionary.
-                    var i,
-                        dictionary = [],
-                        w,
-                        result,
-                        k,
-                        entry = "",
-                        dictSize = 256;
-                    for (i = 0; i < 256; i += 1) {
-                        dictionary[i] = String.fromCharCode(i);
-                    }
-             
-                    w = String.fromCharCode(compressed[0]);
-                    result = w;
-                    for (i = 1; i < compressed.length; i += 1) {
-                        k = compressed[i];
-                        if (dictionary[k]) {
-                            entry = dictionary[k];
-                        } else {
-                            if (k === dictSize) {
-                                entry = w + w.charAt(0);
-                            } else {
-                                return null;
-                            }
-                        }
-             
-                        result += entry;
-             
-                        // Add w+entry[0] to the dictionary.
-                        dictionary[dictSize++] = w + entry.charAt(0);
-             
-                        w = entry;
-                    }
-                    return result;
+    compress: function (uncompressed) {
+        "use strict";
+        // Build the dictionary.
+        var i,
+            dictionary = {},
+            c,
+            wc,
+            w = "",
+            result = [],
+            dictSize = 256;
+        for (i = 0; i < 256; i += 1) {
+            dictionary[String.fromCharCode(i)] = i;
+        }
+
+        for (i = 0; i < uncompressed.length; i += 1) {
+
+            c = uncompressed.charAt(i);
+            wc = w + c;
+            if (dictionary[wc]) {
+                w = wc;
+            } else {
+                result.push(dictionary[w]);
+                // Add wc to the dictionary.
+                dictionary[wc] = dictSize++;
+                w = String(c);
+            }
+        }
+
+        // Output the code for w.
+        if (w !== "") {
+            result.push(dictionary[w]);
+        }
+        return result;
+    },
+
+    decompress: function (compressed) {
+        "use strict";
+        // Build the dictionary.
+        var i,
+            dictionary = [],
+            w,
+            result,
+            k,
+            entry = "",
+            dictSize = 256;
+        for (i = 0; i < 256; i += 1) {
+            dictionary[i] = String.fromCharCode(i);
+        }
+
+        w = String.fromCharCode(compressed[0]);
+        result = w;
+        for (i = 1; i < compressed.length; i += 1) {
+            k = compressed[i];
+            if (dictionary[k]) {
+                entry = dictionary[k];
+            } else {
+                if (k === dictSize) {
+                    entry = w + w.charAt(0);
+                } else {
+                    return null;
                 }
             }
+
+            result += entry;
+
+            // Add w+entry[0] to the dictionary.
+            dictionary[dictSize++] = w + entry.charAt(0);
+
+            w = entry;
+        }
+        return result;
+    }
+};
