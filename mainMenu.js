aa6c21b4c15b3979d48e3fc2acafadda7977d542
Jonas Almeida <jalmeida@uab.edu>
2013-03-05 11:41:50 -0600

    calling googlecode scripts with https

--- mainMenu.js
+++ mainMenu.js
@@ -17,10 +17,10 @@ function handleDownloadImage(ev) {
 (function(){
 
 	var listOfModules={
-		'Hello world':function(evt){imagejs.loadModule('http://imagejs.googlecode.com/git/helloWorld.js')},
-		'Chromomarkers':function(evt){imagejs.loadModule('http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js')},
-		'Count Shapes':function(evt){imagejs.loadModules(['http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','http://module.imagejs.googlecode.com/git/mathbiol.countshapes.js'])},
-		'KI67 analysis':function(evt){imagejs.loadModules(['http://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','http://module.imagejs.googlecode.com/git/ki67.js'])}
+		'Hello world':function(evt){imagejs.loadModule('https://imagejs.googlecode.com/git/helloWorld.js')},
+		'Chromomarkers':function(evt){imagejs.loadModule('https://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js')},
+		'Count Shapes':function(evt){imagejs.loadModules(['https://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','https://module.imagejs.googlecode.com/git/mathbiol.countshapes.js'])},
+		'KI67 analysis':function(evt){imagejs.loadModules(['https://module.imagejs.googlecode.com/git/mathbiol.chromomarkers.js','https://module.imagejs.googlecode.com/git/ki67.js'])}
 	}
 
 	var menu={
