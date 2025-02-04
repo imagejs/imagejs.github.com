d589828d9df205034ad159f62fe927f9daa9125a
Jonas Almeida <jalmeida@mathbiol.org>
2025-02-04 17:01:16 -0500

    Update imagejs.js

--- imagejs.js
+++ imagejs.js
@@ -101,7 +101,7 @@ menu:function(x,id){
 	// process a menu structure
 	var f = jmat.fieldnames(x);
 	if(!jmat.gId('menu '+id)){var sel = jmat.cEl('select','menu '+id)} // create if it doesn't exist
-	else{jmat.gId('menu '+id)=jmat.gId('menu '+id).splice(0,0)} // else clear SELECT options 
+	// else{jmat.gId('menu '+id)=jmat.gId('menu '+id).splice(0,0)} // else clear SELECT options 
 	//jmat.gId('menu').appendChild(sel);
 	var opt = jmat.cEl('option','option '+id);opt.textContent=id; // menu name at the top
 	sel.appendChild(opt);
